f8b0109fcd0fce85c7237cbcbeb6da2569c95ba0
Lhomme <adrien.bournat@epitech.eu>
2017-02-11 02:30:34 +0100

    code64 update

--- code64.asm
+++ code64.asm
@@ -1,3 +1,6 @@
+
+extrn ExitProcess: PROC
+
 .data
 
 .code
@@ -18,22 +21,49 @@ main proc
 	local yhaS[16]:BYTE
 	local msgYhaS[16]:BYTE
 
-	; Get Kernel Base
+	;--------------------------------------------------
+	; 
+	; 	- Get Kernel Base
+	;
+	;--------------------------------------------------
+
 	xor rax, rax
-	mov rax, gs:[eax+60h]				; Get PEB
-	mov rax, [rax+18h]					; PEB_LDR_DATA
-	mov rax, [rax+20h]					; InMemoryOrderModuleList
-	mov rax, [rax]						; Second entry
-	mov rax, [rax]						; third entry
-	mov rax, [rax+20h]					; Get base addr
+	; Get PEB
+	mov rax, gs:[eax+60h]
+	; PEB_LDR_DATA
+	mov rax, [rax+18h]
+	; InMemoryOrderModuleList, first entry
+	mov rax, [rax+20h]
+	; Second entry
+	mov rax, [rax]
+	; Third entry (Kernel32)
+	mov rax, [rax]
+	; Get base addr
+	mov rax, [rax+20h]
+
 	mov kernelBase, rax
 	mov rbx, rax
 
-	; Get Export dir
-	add bx, WORD PTR [rbx+3ch]			; Get new exe addr
-	mov ebx, DWORD PTR [rbx+88h]		; Export dir RVA
+	;--------------------------------------------------
+	; 
+	; 	- Get Export Dir
+	;
+	;--------------------------------------------------
+
+	; New exe addr (DOS HEADER)
+	add bx, WORD PTR [rbx+3ch]
+	; Export Dir RVA addr
+	mov ebx, DWORD PTR [rbx+88h]
+	; Add KernelBase
 	add rbx, kernelBase
-	mov exportDir, rbx					
+	mov exportDir, rbx
+
+	;--------------------------------------------------
+	; 
+	; 	- Nb functions
+	;	- Nb Names
+	;
+	;--------------------------------------------------
 
 	; Nb functions
 	xor rdi, rdi
@@ -45,6 +75,13 @@ main proc
 	mov edi, DWORD PTR [rbx+18h]
 	mov nbNames, rdi
 
+	;--------------------------------------------------
+	; 
+	; 	- Find GetProcAddress
+	;
+	;--------------------------------------------------
+	
+	; String "GetProcAddress"
 	lea rdi, getProcS
 	mov DWORD PTR [rdi], 50746547h
 	mov DWORD PTR [rdi+04h], 41636f72h
@@ -53,29 +90,32 @@ main proc
 	
 	xor rcx, rcx
 LOOPB:
+	; Get AddressOfNames addr
 	mov rsi, exportDir
 	mov esi, DWORD PTR [rsi+20h]		; AddressOfNames
 	add rsi, kernelBase
 
-	; Set name addr to rdi to cmp
+	; Get addr of name to cmp
 	mov rdi, rcx
 	shl rdi, 2
 	add rsi, rdi
 	mov edi, DWORD PTR [rsi]
 	add rdi, kernelBase
 
+	; Cmp name with "GetProcAddress"
 	lea rsi, getProcS
 	push rcx
 	mov rcx, 14
 	cld
 	repe cmpsb
 	pop rcx
+
+	; If not equal go to next name
 	jne LOOPNEXT
 
-	; GetProc found
+	; If equal, Get addr of functions with AddressOfNameOrdinales
 	mov rsi, exportDir
-	mov esi, [rsi+24h]					; AddressOfNameOrdinals
-										; Each ordi => WORD
+	mov esi, [rsi+24h]
 	add rsi, kernelBase
 	xor rdi, rdi
 	mov di, WORD PTR [rcx * 2 + rsi]	; Ordinal in rdi
@@ -97,6 +137,12 @@ LOOPNEXT:
 
 ADDRPROCOK:
 
+	;--------------------------------------------------
+	; 
+	; 	- Find "LoadLibraryA" with GetProcAddress
+	;
+	;--------------------------------------------------
+
 	lea rdi, loadLibS
 	mov [rdi], DWORD PTR 64616f4ch
 	mov [rdi+4h], DWORD PTR 7262694ch
@@ -108,17 +154,31 @@ ADDRPROCOK:
 	lea rdx, loadLibS
 	mov rdi, getProcAddr
 	call rdi
+	;call GetProcAddress
+	add	rsp, 28h
 	mov loadLibAddr, rax
 	
-	lea rdi, loadLibS
+	;--------------------------------------------------
+	; 
+	; 	- Load user32.dll with LoadLibraryA
+	;
+	;--------------------------------------------------
+
+	lea rdi, user32S
 	mov [rdi], DWORD PTR 72657355h
 	mov [rdi+4h], DWORD PTR 642e3233h
 	mov [rdi+8h], DWORD PTR 00006c6ch
 
 	;sub rsp,28h
-	lea rcx, loadLibs
+	lea rcx, user32S
 	call rax
 	;add rsp,28h
+
+	;--------------------------------------------------
+	; 
+	; 	- Find "MessageBoxA" with GetProcAddress
+	;
+	;--------------------------------------------------
 
 	lea rdi, msgBoxS
 	mov [rdi], DWORD PTR 7373654dh
@@ -132,27 +192,35 @@ ADDRPROCOK:
 	call rax
 	;add rsp,28h
 
+	;--------------------------------------------------
+	; 
+	; 	- Call MessageBoxA
+	;
+	;--------------------------------------------------
+
 	; Build title "Yharnam"
-	
 	lea rdi, yhaS
 	mov [rdi], DWORD PTR 72616859h
 	mov [rdi+4h], DWORD PTR 006d616eh
 
 	; Build msg "Infected !"
 	; 496e6665 63746564 20210000
-
 	lea rdi, msgYhaS
 	mov [rdi], DWORD PTR 65666e49h
 	mov [rdi+4h], DWORD PTR 64657463h
 	mov [rdi+8h], DWORD PTR 00002120h
 
-	sub rsp,28h
+	;sub rsp,28h
 	mov rcx, 0
 	lea rdx, msgYhaS
 	lea r8, yhaS
 	mov r9d, 0
 	call rax
+	add rsp,28h
 
-	ret
+	mov ecx, 0
+	call ExitProcess
+	
 main endp
+
 END
